8344ab32f4276a5566707e87e8c981b663c54635
L-hying <1638734916@qq.com>
2020-11-18 18:08:45 +0800

    add annotation for AlarmReceiver

--- app/src/main/java/net/micode/notes/ui/AlarmReceiver.java
+++ app/src/main/java/net/micode/notes/ui/AlarmReceiver.java
@@ -19,12 +19,15 @@ package net.micode.notes.ui;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
+/*
+* 警告接收者，继承于广播接收者
+* 通过intent接收来自AlarmAlertActivity广播传的值
+* */
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         intent.setClass(context, AlarmAlertActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  //在新任务中启动该活动
         context.startActivity(intent);
     }
 }
