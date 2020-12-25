91c7b0e909c0918be15b5564d83371971809c9c5
Plumk <1285847284@qq.com>
2020-12-25 13:08:52 +0800

    fix bug

--- app/src/main/java/net/micode/notes/ui/CreateAlarm.java
+++ app/src/main/java/net/micode/notes/ui/CreateAlarm.java
@@ -1,13 +1,13 @@
 package net.micode.notes.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.provider.AlarmClock;
 
 class CreateAlarm extends Activity {
 
-    public void addAlarm(int hour, int minutes) {
-        String message = "便签提醒";
+    public void addAlarm(int hour, int minutes, Context context, String message) {
         Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
                 //闹钟的小时
                 .putExtra(AlarmClock.EXTRA_HOUR, hour)
@@ -19,7 +19,7 @@ class CreateAlarm extends Activity {
                 .putExtra(AlarmClock.EXTRA_VIBRATE, true)
                 //如果为true，则调用startActivity()不会进入手机的闹钟设置界面
                 .putExtra(AlarmClock.EXTRA_SKIP_UI, true);
-        startActivity(intent);
+        context.startActivity(intent);
         //Toast.makeText(getApplicationContext(),"设置完毕",Toast.LENGTH_SHORT).show();
     }
 
