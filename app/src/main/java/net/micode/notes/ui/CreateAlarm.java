9da26ec9c338e131e4a2459c017b07184b90f12e
10914 <1091497501@qq.com>
2020-12-25 12:38:32 +0800

    系统闹钟封装为独立的类

---
+++ app/src/main/java/net/micode/notes/ui/CreateAlarm.java
@@ -0,0 +1,26 @@
+package net.micode.notes.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.AlarmClock;
+
+class CreateAlarm extends Activity {
+
+    public void addAlarm(int hour, int minutes) {
+        String message = "便签提醒";
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                //闹钟的小时
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                //闹钟的分钟
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+                //响铃时提示的信息
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                //用于指定该闹铃触发时是否振动
+                .putExtra(AlarmClock.EXTRA_VIBRATE, true)
+                //如果为true，则调用startActivity()不会进入手机的闹钟设置界面
+                .putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        startActivity(intent);
+        //Toast.makeText(getApplicationContext(),"设置完毕",Toast.LENGTH_SHORT).show();
+    }
+
+}
